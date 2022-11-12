8330c0dfe1a5c1112c23c2fa0f48e39e933c9ed9
Siddharth Belbase <siddharthbelbase@gmail.com>
2022-11-12 17:34:31 +0545

    feat: pass Ondelete props from App.js to Task.js

--- src/App.js
+++ src/App.js
@@ -27,10 +27,15 @@ function App() {
     ]
   )
 
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header title="Mero Task Tracker" />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
